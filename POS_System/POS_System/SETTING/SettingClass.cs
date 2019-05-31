f6574b577a8dc670d2c03aad61cdd609989dcfd4
aymenbatool <aymenbatool21@gmail.com.com>
2019-05-31 20:46:46 +0500

    adding initial project

---
+++ POS_System/POS_System/SETTING/SettingClass.cs
@@ -0,0 +1,17 @@
+﻿using System.Data.SqlClient;
+
+namespace POS_System
+{
+    class SettingClass
+    {
+        static string conStr = System.Configuration.ConfigurationManager.ConnectionStrings["Data Source=DESKTOP-TDJD3P5/MSSQLSERVER14;Initial Catalog=pos_system;Integrated Security=True"].ConnectionString;
+        SqlConnection con = new SqlConnection(conStr);
+        public void update_Company(int _id, string _name)
+        {
+            SqlCommand com = new SqlCommand("UPDATE tbl_Company SET Name='" + _name + "' WHERE ID='" + _id + "'", con);
+            con.Open();
+            com.ExecuteNonQuery();
+            con.Close();
+        }
+    }
+}
